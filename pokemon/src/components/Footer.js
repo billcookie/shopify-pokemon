0b04fde24e65d7fde64699387ba01aca37e03cbc
Josh Chappelow <jdchappelow@gmail.com>
2022-09-12 14:40:04 +0900

    fixed footer

--- pokemon/src/components/Footer.js
+++ pokemon/src/components/Footer.js
@@ -2,41 +2,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer
-      class="text-center text-white mt-auto"
-      style="background-color: white;"
-    >
-      <div class="container pt-4">
-        <section class="mb-4">
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://github.com/lw-a"
-            target="_blank"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i class="fab fa-github" style="color: black"></i>
-          </a>
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://github.com/jchapps"
-            target="_blank"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i class="fab fa-github" style="color: green"></i>
-          </a>
-          <a
-            class="btn btn-link btn-floating btn-lg m-1"
-            href="https://github.com/billcookie"
-            target="_blank"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i class="fab fa-github" style="color: orange"></i>
-          </a>
-        </section>
+    <div className="footer">
+      <div className="footer-links">
+        <a href="https://github.com/lw-a" target="_blank">
+          <i className="adam fab fa-github"></i>
+        </a>
+        <a href="https://github.com/billcookie" target="_blank">
+          <i className="bill fab fa-github"></i>
+        </a>
+        <a href="https://github.com/jchapps" target="_blank">
+          <i className=" josh fab fa-github"></i>
+        </a>
       </div>
-    </footer>
+    </div>
   );
 }
